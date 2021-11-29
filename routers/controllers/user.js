bf5ae0d032c0c5f966fd2f7408a26703aa5aa464
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-29 22:58:11 +0300

    bycrpt-token,jwt

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -1,49 +1,68 @@
 const usermodel = require('./../../db/models/user')
+const bcrypt = require("bcrypt");
+var jwt = require('jsonwebtoken');
 
 const signup= (req, res) =>{
     const { email, password, role } = req.body;
-    
-    const newuser = new usermodel({
-        email,
-        password,
-        role
+
+    const SALT = Number(process.env.SALT);
+    const savedEmail = email.toLowerCase();
+    const hashedPassword = await bcrypt.hash(password, SALT);
+    const newUser = new usermodel({
+      email: savedEmail,
+      password: hashedPassword,
+      role
+    });
+  
+    newUser
+      .save()
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
       });
-    
-      newuser 
-        .save()
-        .then((result) => {
-          res.json(result);
-        })
-        .catch((err) => {
-          res.send(err);
-        });
-    }
-    const login = (req, res) =>{
-        const { email, password } = req.body;
-
-    }
+  };
+  
+  
+  const login = (req, res) => {
+    const { email, password } = req.body;
+    const SECRET_KEY = process.env.SECRET_KEY;
     usermodel
-
-       .findOne({ email})
-        .then((result) => {
-          if (result) {
-            if ( result.email==email) {
-                if ( result.password==password) {
-    
-                    res.status(200).json(result);
-                } else {
-                  res.status(400).json("invalid email or password");
-                }
-              } else {
-                res.status(400).json("invalid email or password");
-              }
-            } else {
-              res.status(400).json("email do not found");
+      .findOne({ email })
+      .then(async (result) => {
+        if (result) {
+          if (email === result.email) {
+  
+            const payload={
+              role:result.role
             }
-          })
-          .catch((err) => {
-            res.status(400).json(err);
-          });
-    
-
+  
+            const options={
+              expiresIn: 60*60
+            }
+  
+            const token =await jwt.sign(payload, SECRET_KEY, options)
+            console.log(token);
+  
+         const unhashPassword = await bcrypt.compare(password, result.password)
+  
+            if (unhashPassword) {
+              res.status(200).json(result);
+            } else {
+              res.status(400).json("invalid email or password");
+            }
+          } else {
+            res.status(400).json("invalid email or password");
+          }
+        } else {
+          res.status(400).json("email do not found");
+        }
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  };
       module.exports= { signup , login }
+
+      
