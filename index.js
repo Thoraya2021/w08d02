98493862757ab38b5acec98c2e3abb17926df9ca
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-29 16:45:26 +0300

    main write code and first  steps for run localhost

---
+++ index.js
@@ -0,0 +1,23 @@
+//import to all package what  i install 
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+//run and config express 
+const app = express();
+dotenv.config();
+
+
+
+//middel ware uses what i install and build should write here 
+app.use(express.json());
+app.use(cors());
+
+
+
+
+//here to run locallhost 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running at port ${PORT}`);
+});
